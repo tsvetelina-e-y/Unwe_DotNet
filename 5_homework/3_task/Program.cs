4d084b12afa91cdcd8e58dd2857e034646934868
Tsvetelina <tsvetelina@ever.co>
2021-03-27 23:56:28 +0200

    added exercise task 1 from exercise 7

---
+++ 5_homework/3_task/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            List<List<int>> parts = new List<List<int>>();
+
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                List<int> row = new List<int>(matrixSize);
+
+                for (int k = 0; k < matrixSize; k++)
+                {
+
+                    if (k % 2 == 0)
+                    {
+                        row.Add((i + 1) + k * matrixSize);
+                    }
+                    else
+                    {
+                        row.Add(k * matrixSize + matrixSize - i);
+                    }
+                }
+
+                parts.Add(row);
+            }
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int k = 0; k < matrixSize; k++)
+                {
+
+                    Console.Write(parts[i][k] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 7_exercise/7_exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _7_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"E:\unwe\.net\exercices\Example.txt");
+            using(sw)
+            {
+                Console.WriteLine("Press any key to begin and esc to stop");
+                Console.ReadKey(true);
+
+                while(Console.ReadKey(true).Key != ConsoleKey.Escape)
+                {
+                    sw.WriteLine(Console.ReadLine());
+                }
+            }
+
+            sw.Close();
+
+            StreamReader sr = new StreamReader(@"E:\unwe\.net\exercices\Example.txt");
+            using(sr)
+            {
+                Console.WriteLine("File Content:");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+    }
+}
